afa0dda015b5b087b5d34d896f21fc51077d7c3e
thorraythorray <helloleidong@gmail.com>
2021-04-26 18:57:55 +0800

    add

--- repair-online-modules/pages/user_pages/guide/index.js
+++ repair-online-modules/pages/user_pages/guide/index.js
@@ -1,16 +1,18 @@
 
 // pages/funny/index.js
+const app = getApp();
+const common = require("../../../utils/common.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    contents: [{
-      "title": "校园网报修指南",
-      "date": "2017-03-18",
-      "content": "PC端操作请登陆http://itss.wxc.edu.cn/进行报修，或进入网络中心主页，在导航栏点击“运维报修”。学生和教师的用户名、密码与学校“信息门户”的用户名和密码一致(学生账户是学号，教工账户是教工号，如若未修改密码，则默认为身份证后8位)。 "
-    }]
+    // contents: [{
+    //   "title": "校园网报修指南",
+    //   "date": "2017-03-18",
+    //   "content": "PC端操作请登陆http://itss.wxc.edu.cn/进行报修，或进入网络中心主页，在导航栏点击“运维报修”。学生和教师的用户名、密码与学校“信息门户”的用户名和密码一致(学生账户是学号，教工账户是教工号，如若未修改密码，则默认为身份证后8位)。 "
+    // }]
   },
 
   /**
@@ -18,6 +20,7 @@ Page({
    */
   onLoad: function (options) {
     let that = this;
+    var notice = common.getNotice()
     wx.getSystemInfo({
       success (res) {
         console.log(res.model)
@@ -27,8 +30,10 @@ Page({
         console.log(res.language)
         console.log(res.version)
         console.log(res.platform)
+        
         that.setData({
-          windowHeight: res.windowHeight
+          windowHeight: res.windowHeight,
+          notice: notice
         })
       }
     })
